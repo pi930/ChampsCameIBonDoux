f5699f4c2b8248c947b3a2554cc8a5e45da2ec2d
pi930 <t.pierrard.131.198@outlook.fr>
2026-02-17 17:45:35 +0100

    Refonte page Mes commandes + affichage statut_retrait + lien Voir la commande

--- resources/views/commandes/index.blade.php
+++ resources/views/commandes/index.blade.php
@@ -6,30 +6,60 @@
     </x-slot>
 
     <div class="py-8">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+
             @if ($commandes->isEmpty())
-                <p>Vous n'avez encore aucune commande.</p>
+                <p class="text-gray-600">Vous n'avez encore aucune commande.</p>
             @else
-                <table class="min-w-full bg-white border">
-                    <thead>
+                <table class="min-w-full bg-white border rounded-lg overflow-hidden shadow">
+                    <thead class="bg-gray-100">
                         <tr>
                             <th class="px-4 py-2 border">#</th>
-                            <th class="px-4 py-2 border">Statut</th>
+                            <th class="px-4 py-2 border">Statut retrait</th>
                             <th class="px-4 py-2 border">Total</th>
                             <th class="px-4 py-2 border">Créée le</th>
                             <th class="px-4 py-2 border">Actions</th>
                         </tr>
                     </thead>
+
                     <tbody>
                         @foreach ($commandes as $commande)
-                            <tr>
-                                <td class="px-4 py-2 border">{{ $commande->id }}</td>
-                                <td class="px-4 py-2 border">{{ $commande->statut }}</td>
-                                <td class="px-4 py-2 border">{{ $commande->total }} €</td>
-                                <td class="px-4 py-2 border">{{ $commande->created_at }}</td>
-                                <td class="px-4 py-2 border">
+                            <tr class="hover:bg-gray-50">
+                                <td class="px-4 py-2 border text-center">
+                                    {{ $commande->id }}
+                                </td>
+
+                                <td class="px-4 py-2 border text-center">
+                                    @if ($commande->statut_retrait === 'en_attente')
+                                        <span class="px-2 py-1 text-sm bg-yellow-200 text-yellow-800 rounded">
+                                            En attente
+                                        </span>
+                                    @elseif ($commande->statut_retrait === 'pret')
+                                        <span class="px-2 py-1 text-sm bg-blue-200 text-blue-800 rounded">
+                                            Prête
+                                        </span>
+                                    @elseif ($commande->statut_retrait === 'recuperee')
+                                        <span class="px-2 py-1 text-sm bg-green-200 text-green-800 rounded">
+                                            Récupérée
+                                        </span>
+                                    @else
+                                        <span class="px-2 py-1 text-sm bg-gray-200 text-gray-800 rounded">
+                                            Inconnu
+                                        </span>
+                                    @endif
+                                </td>
+
+                                <td class="px-4 py-2 border text-center">
+                                    {{ number_format($commande->total, 2, ',', ' ') }} €
+                                </td>
+
+                                <td class="px-4 py-2 border text-center">
+                                    {{ $commande->created_at->format('d/m/Y H:i') }}
+                                </td>
+
+                                <td class="px-4 py-2 border text-center">
                                     <a href="{{ route('commande.recuperee', $commande->id) }}"
-                                       class="text-blue-600 underline">
+                                       class="text-blue-600 hover:text-blue-800 underline">
                                         Voir la commande
                                     </a>
                                 </td>
@@ -38,7 +68,7 @@
                     </tbody>
                 </table>
             @endif
+
         </div>
     </div>
 </x-app-layout>
-
